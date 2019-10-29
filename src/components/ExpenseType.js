c35d76820e37b7a7d5fe5afcc3fe551ce2928200
David Glines <dglines86@gmail.com>
2019-10-29 11:54:49 -0400

    added expand icon, removed add buton for expense types, now click on type to add

--- src/components/ExpenseType.js
+++ src/components/ExpenseType.js
@@ -6,23 +6,42 @@ import MaterialIcon from "material-icons-react"
 
 const ExpenseType = props => {
   const [isClicked, setClicked] = useState(false)
+  const [icon, setIcon] = useState("chevron_right")
+  //var icon = "chevron_right"
+  const handleExpand = () => {
+    console.log(isClicked)
+    setClicked(!isClicked)
+    isClicked ? setIcon("chevron_right") : setIcon("chevron_left")
+  }
+
+  const handleAdd = () => {
+    props.history.push("/expense/" + props.type + "/0")
+  }
 
   return (
     <div>
-      <div className="expenseType" onClick={() => setClicked(!isClicked)}>
-        <Link to={"/type/" + props.id} className="edit_icon">
-          <MaterialIcon icon="edit" size="15" />
-        </Link>
-        <span>{props.type}</span>
-        <span className="planned">{Number(props.budget).toFixed(2)}</span>
-        <span className="actual">{Number(props.balance).toFixed(2)}</span>
-        <span className="diff">
-          {Number(props.budget - props.balance).toFixed(2)}
-        </span>
-        <span>
+      <div className="expenseType">
+        {/*<span>
           <Link to={"/expense/" + props.type + "/0"}>
             <MaterialIcon icon="add_circle" size="15" />
           </Link>
+        </span>*/}
+        {isClicked && (
+          <MaterialIcon icon="expand_less" onClick={handleExpand} />
+        )}
+        {!isClicked && (
+          <MaterialIcon icon="expand_more" onClick={handleExpand} />
+        )}
+        <Link to={"/type/" + props.id} className="edit_icon">
+          <MaterialIcon icon="edit" size="15" />
+        </Link>
+        <span className="clickable_expense_type" onClick={handleAdd}>
+          <span>{props.type}</span>
+          <span className="planned">{Number(props.budget).toFixed(2)}</span>
+          <span className="actual">{Number(props.balance).toFixed(2)}</span>
+          <span className="diff">
+            {Number(props.budget - props.balance).toFixed(2)}
+          </span>
         </span>
       </div>
       {isClicked && <ExpenseList expenseType={props.type} />}
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,12 @@ import Balance from "./Balance"
 import ExpenseTypeList from "./ExpenseTypeList"
 import { Link } from "react-router-dom"
 import MaterialIcon from "material-icons-react"
+import logo from "../img/fin_logo_2.ico"
 
 const Home = props => {
   return (
     <div>
+      <img src={logo} alt="" height="50" width="50" />
       <Balance />
       <ExpenseTypeList history={props.history} />
       <br />
